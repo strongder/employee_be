ac0d640c019232efc985502742fa83c51d0e8917
thanhnv <thanhvn@lsd>
2025-07-04 16:48:06 +0700

    fix login

--- src/main/java/com/example/employee/controller/AuthController.java
+++ src/main/java/com/example/employee/controller/AuthController.java
@@ -1,5 +1,7 @@
 package com.example.employee.controller;
 
+import com.example.employee.model.Account;
+import com.example.employee.repository.AccountRepository;
 import com.example.employee.utils.JwtUtils;
 import com.example.employee.dtos.request.AuthRequest;
 import com.example.employee.dtos.response.ApiResponse;
@@ -26,10 +28,17 @@ public class AuthController {
     private AuthenticationManager authenticationManager;
     @Autowired
     private JwtUtils jwtUtils;
+    @Autowired
+    private AccountRepository accountRepository;
+
     @PostMapping("/login")
     public ResponseEntity<AuthResponse> login(@RequestBody AuthRequest authRequest) {
         log.info("authRequest: {}", authRequest);
-
+        Account account = accountRepository.findByUsername(authRequest.getUsername())
+                .orElseThrow(() -> new RuntimeException("Tài khoản không tồn tại"));
+        if(!account.getStatus().equals("ACTIVE") || account.isDeleted()) {
+            return new ResponseEntity<>(new AuthResponse("Tài khoản không hoạt động", null), HttpStatus.FORBIDDEN);
+        }
         Authentication authentication = authenticationManager.authenticate(
                 new UsernamePasswordAuthenticationToken(authRequest.getUsername(), authRequest.getPassword())
         );
